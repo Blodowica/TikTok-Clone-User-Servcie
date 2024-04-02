be6e06425668217bf5cb81dbef3853dc2ede699c
Brandon Lodowica <90032396+Blodowica@users.noreply.github.com>
2024-04-02 17:57:11 +0200

    Added user CRUD functionality +db

---
+++ TikTok-Clone-User-Service/Models/Follower.cs
@@ -0,0 +1,16 @@
+﻿namespace TikTok_Clone_User_Service.Models
+{
+    public class Follower
+    {
+        public int Id { get; set; }
+
+        // the person following the user
+        public int followerUserId { get; set; }
+
+        //the user that is being followed
+        public int UserId { get; set; }
+
+       public User User { get; set; }
+
+    }
+}
